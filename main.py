d529ebe7aeb51d4dfca0f5f064e98a1d13867f3c
VORCITY <vorcity99@gmail.com>
2024-07-26 04:56:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.types import CallbackQuery
 
-
-
 API_ID = "28061494"
 API_HASH = "5f7c625731e89fa00a6b3906e1f894a7"
 BOT_TOKEN = "5834355850:AAEagWKMd80wfOk5Y__z7jq96eZ4y-VgbdM"
@@ -16,17 +14,16 @@ MTG33 = Client(
 )
 
 START_BUTTONS = [[
-   InlineKeyboardButton("📢 JOIN CHANNEL FOR REPO", url="t.me/MTG33BOTZ"
+    InlineKeyboardButton("📢 JOIN CHANNEL FOR REPO", url="t.me/MTG33BOTZ")
 ]]
 
 @MTG33.on_message(filters.command("start"))
 async def start_cmd(client, message):
     await message.reply_text(
-        text="Hey !! {message.from_user.mention} ... I am a pyrogram Bot Created By @MTG33"),
+        text=f"Hey !! {message.from_user.mention} ... I am a pyrogram Bot Created By @MTG33",
         reply_markup=InlineKeyboardMarkup(START_BUTTONS)
     )
 
-
-print(" © BOT DEPLOYED © ")
+print("© BOT DEPLOYED ©")
 
 MTG33.run()
